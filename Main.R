7e18f68ec6d1774c802b4fe83c2b66e5ac54b4e8
Michael Dietze <dietze@bu.edu>
2022-09-16 09:17:16 -0400

    turn comments into print statements to make it easier to debug Github Actions

--- Main.R
+++ Main.R
@@ -17,35 +17,35 @@ team_info <- list(team_name = "air2waterSat_MCD",
                                         electronicMailAddress = "dietze@bu.edu"))
                   )
 
-## Load required functions
+print("## Load required functions")
 if(file.exists("01_download_data.R"))      source("01_download_data.R")
 if(file.exists("02_calibrate_forecast.R")) source("02_calibrate_forecast.R")
 if(file.exists("03_run_forecast.R"))       source("03_run_forecast.R")
 if(file.exists("04_submit_forecast.R"))    source("04_submit_forecast.R")
 
-### Step 1: Download Required Data
+print("### Step 1: Download Required Data")
 target     <- download_targets()       ## Y variables
 site_data  <- download_site_meta()
 target     <- merge_met_past(target)   ## append met data (X) into target file
 met_future <- download_met_forecast(forecast_date) ## Weather forecast (future X)
 
-## visual check of data
+print("## visual check of data")
 ggplot(target, aes(x = temperature, y = air_temperature)) +
   geom_point() +
   labs(x = "NEON water temperature (C)", y = "NOAA air temperature (C)") +
   facet_wrap(~site_id)
 
-### Step 2: Calibrate forecast model
+print("### Step 2: Calibrate forecast model")
 model <- calibrate_forecast(target)
 
-### Step 3: Make a forecast into the future
+print("### Step 3: Make a forecast into the future")
 forecast <- run_forecast(model,met_future,site_data)
 
-#Visualize forecast.  Is it reasonable?
+print("#Visualize forecast.  Is it reasonable?")
 forecast %>% 
   ggplot(aes(x = time, y = predicted, group = ensemble)) +
   geom_line() +
   facet_grid(variable~site_id, scale ="free")
 
-### Step 4: Save and submit forecast and metadata
+print("### Step 4: Save and submit forecast and metadata")
 submit_forecast(forecast,team_info,submit=FALSE)
